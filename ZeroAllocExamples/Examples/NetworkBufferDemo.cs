2c96a68ac8d819367802b0a85c0711a6b4811e0c
UglukFearless <UglukFearless@mail.ru>
2026-03-11 00:05:26 +0700

    Add example with compare  creation byte array and array from pool

---
+++ ZeroAllocExamples/Examples/NetworkBufferDemo.cs
@@ -0,0 +1,46 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Buffers;
+
+namespace ZeroAllocExamples.Examples;
+
+[MemoryDiagnoser]
+public class NetworkBufferDemo : IExample
+{
+    private const int BufferSize = 4096;
+
+    [GlobalSetup]
+    public void Setup() {}
+
+    [Benchmark(Baseline = true)]
+    public int ProcessPacketAsNewArray()
+    {
+        var buffer = new byte[BufferSize];
+        int checksum = 0;
+        for (int i = 0; i < buffer.Length; i++)
+        {
+            buffer[i] = 1;
+            checksum += buffer[i];
+        }
+        return checksum;
+    }
+
+    [Benchmark]
+    public int ProcessPacketAsArrayPool()
+    {
+        var buffer = ArrayPool<byte>.Shared.Rent(BufferSize);
+        try
+        {
+            int checksum = 0;
+            for (int i = 0; i < buffer.Length; i++)
+            {
+                buffer[i] = 1;
+                checksum += buffer[i];
+            }
+            return checksum;
+        }
+        finally
+        {
+            ArrayPool<byte>.Shared.Return(buffer);
+        }
+    }
+}
